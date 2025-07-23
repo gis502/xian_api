cdce9401a40700a27da98376e66748ee0d892a88
Qiuhuawa <1010565096@qq.com>
2025-07-23 20:19:44 +0800

    cc20250723

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/AroundAnalysisController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/AroundAnalysisController.java
@@ -2,35 +2,31 @@ package com.ruoyi.web.controller.system;
 
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.system.service.IGeologicalDisasterHideService;
-import io.swagger.annotations.Api;
-import org.springframework.stereotype.Controller;
+import com.ruoyi.system.service.IGeologicalDisasterRiskService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.List;
-
-/**
- * @author: xiaodemos
- * @date: 2025-07-22 19:15
- * @description: 风险区控制类
- */
-
 
 @RestController
-public class GeologicalDisasterHideController {
+@RequestMapping("/around_analysis")
+public class AroundAnalysisController {
 
     @Resource
+    private IGeologicalDisasterRiskService geologicalDisasterRiskService;
     private IGeologicalDisasterHideService geologicalDisasterHideService;
 
+    @GetMapping("/risk_disaster")
+    public AjaxResult getGeologicalDisasterRiskList()
+    {
+        return AjaxResult.success(geologicalDisasterRiskService.getGeologicalDisasterRiskList());
+    }
 
-    @GetMapping("/hide/disaster")
+    @GetMapping("/hide_disaster")
     public AjaxResult getGeologicalDisasterHideList()
     {
         return AjaxResult.success(geologicalDisasterHideService.getGeologicalDisasterHideList());
     }
-
 
 }
