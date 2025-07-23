a873d0813e4fe35e34fcd2ccb9c188edf417bb12
YQ-Bot01 <3411616574@qq.com>
2025-07-23 18:47:39 +0800

    20250723-字段还原

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/XianDisasterRain.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/XianDisasterRain.java
@@ -8,26 +8,26 @@ import lombok.Data;
 @Data
 @TableName("xian_disaster_rain")
 public class XianDisasterRain {
-//  @TableField("disaster_name")
-//  private String disasterName;
-//  @TableField("occur_time")
-//  private java.sql.Timestamp occurTime;
-//  @TableField("rainfall")
-//  private String rainfall;
-//  @TableField("duration")
-//  private String duration;
-//  @TableField("position")
-//  private String position;
-//  @TableField("create_time")
-//  private java.sql.Timestamp createTime;
-//  @TableField("update_time")
-//  private java.sql.Timestamp updateTime;
-//  @TableField("is_deleted")
-//  private String isDeleted;
+  @TableField("disaster_name")
+  private String disasterName;
+  @TableField("occur_time")
+  private java.sql.Timestamp occurTime;
+  @TableField("rainfall")
+  private String rainfall;
+  @TableField("duration")
+  private String duration;
+  @TableField("position")
+  private String position;
+  @TableField("create_time")
+  private java.sql.Timestamp createTime;
+  @TableField("update_time")
+  private java.sql.Timestamp updateTime;
+  @TableField("is_deleted")
+  private String isDeleted;
   @TableField("geom")
   private String geom;
-//  @TableId
-//  @TableField("disaster_id")
-//  private long disasterId;
+  @TableId
+  @TableField("disaster_id")
+  private long disasterId;
 
 }
--- ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/XianEarthquakeList.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/XianEarthquakeList.java
@@ -14,47 +14,47 @@ import org.n52.jackson.datatype.jts.GeometrySerializer;
 @Data
 @TableName("xian_earthquake_list")
 public class XianEarthquakeList {
-//  @TableId
-//  @TableField("eqid")
-//  private String eqid;
-//  @TableField("earthquake_name")
-//  private String earthquakeName;
-//  @TableField("earthquake_full_name")
-//  private String earthquakeFullName;
-//  @TableField("eq_addr")
-//  private String eqAddr;
+  @TableId
+  @TableField("eqid")
+  private String eqid;
+  @TableField("earthquake_name")
+  private String earthquakeName;
+  @TableField("earthquake_full_name")
+  private String earthquakeFullName;
+  @TableField("eq_addr")
+  private String eqAddr;
 
   @TableField("geom")
   private String geom;
 //
-//  @TableField("intensity")
-//  private String intensity;
-//  @TableField("depth")
-//  private String depth;
-//  @TableField("occurrence_time")
-//  private java.sql.Timestamp occurrenceTime;
-//  @TableField("eq_type")
-//  private String eqType;
-//  @TableField("source")
-//  private String source;
-//  @TableField("eq_addr_code")
-//  private String eqAddrCode;
-//  @TableField("town_code")
-//  private String townCode;
-//
-//  @TableField("is_deleted")
-//  private long isDeleted;
-//  @TableField("magnitude")
-//  private String magnitude;
-//  @TableField("create_time")
-//  private java.sql.Date createTime;
-//  @TableField("update_time")
-//  private java.sql.Date updateTime;
-//  @TableField("district")
-//  private String district;
-//  @TableField("province")
-//  private String province;
-//  @TableField("city")
-//  private String city;
+  @TableField("intensity")
+  private String intensity;
+  @TableField("depth")
+  private String depth;
+  @TableField("occurrence_time")
+  private java.sql.Timestamp occurrenceTime;
+  @TableField("eq_type")
+  private String eqType;
+  @TableField("source")
+  private String source;
+  @TableField("eq_addr_code")
+  private String eqAddrCode;
+  @TableField("town_code")
+  private String townCode;
+
+  @TableField("is_deleted")
+  private long isDeleted;
+  @TableField("magnitude")
+  private String magnitude;
+  @TableField("create_time")
+  private java.sql.Date createTime;
+  @TableField("update_time")
+  private java.sql.Date updateTime;
+  @TableField("district")
+  private String district;
+  @TableField("province")
+  private String province;
+  @TableField("city")
+  private String city;
 
 }
