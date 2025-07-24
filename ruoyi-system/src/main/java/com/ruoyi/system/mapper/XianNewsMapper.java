6c58b1da7500e5bb2758c548c0fb40a8a26fa09a
Smile <3039126061@qq.com>
2025-07-24 18:11:12 +0800

    20250724知识图谱新闻页面

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/XianNewsMapper.java
@@ -0,0 +1,9 @@
+package com.ruoyi.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.system.domain.XianNews;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface XianNewsMapper extends BaseMapper<XianNews> {
+}
