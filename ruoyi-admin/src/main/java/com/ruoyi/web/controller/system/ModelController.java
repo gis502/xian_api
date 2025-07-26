b870c15fb91b112aebe60dd7dea45ff085e5c084
chocoCui <1017185771@qq.com>
2025-07-26 21:33:31 +0800

    地震模型计算接口

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/ModelController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/ModelController.java
@@ -1,19 +1,13 @@
 package com.ruoyi.web.controller.system;
 
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.domain.model.RegisterBody;
-import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.system.domain.dto.ModelGetDataFactorListEntityIdDTO;
 import com.ruoyi.system.domain.vo.FactorVO;
-import com.ruoyi.system.service.IGeologicalDisasterHideService;
 import com.ruoyi.system.service.IModelService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
@@ -46,11 +40,5 @@ public class ModelController {
     {
         return AjaxResult.success(modelService.eqSlideFactorUpdata(request));
     }
-//
-//    @PostMapping("/rainSlideFactorUpdata")
-//    public AjaxResult rainSlideFactorUpdata(@RequestBody List<FactorVO> request)
-//    {
-//        return AjaxResult.success(modelService.rainSlideFactorUpdata(request));
-//    }
 
 }
