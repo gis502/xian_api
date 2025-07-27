07feb8344734e487c9db564bc52485229ab3c7d8
xiaodemos <11165536+xiaodemos@user.noreply.gitee.com>
2025-07-28 01:30:32 +0800

    优化查询所有隐患点速度

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/FactorValueServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/FactorValueServiceImpl.java
@@ -1,19 +1,13 @@
 package com.ruoyi.system.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.ruoyi.common.utils.bean.BeanUtils;
-import com.ruoyi.system.domain.dto.FactorValueDTO;
-import com.ruoyi.system.domain.entity.FactorValue;
+import com.ruoyi.system.domain.dto.BatchHideIdsDTO;
 import com.ruoyi.system.domain.vo.FactorVO;
 import com.ruoyi.system.mapper.FactorValueMapper;
 import com.ruoyi.system.service.IFactorValueService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author: xiaodemos
@@ -34,26 +28,20 @@ public class FactorValueServiceImpl implements IFactorValueService {
         return factorValueMapper.getFactorValueByHideId(hideId);
     }
 
-    // 获取所有因子值表中隐患点Id
+    // 批次查询因子值
     @Override
-    public List<FactorValueDTO> getAllHideId() {
+    public List<FactorVO> getFactorValuesByHideIds(List<Integer> hideIds) {
+        List<FactorVO> factorValuesByHideIds = factorValueMapper.getFactorValuesByHideIds(hideIds);
+        return factorValuesByHideIds;
+    }
 
-        QueryWrapper wrapper = new QueryWrapper();
-        // 筛选 hide_id 列
-        wrapper.select("distinct hide_id");
-        List<FactorValue> list = factorValueMapper.selectList(wrapper);
-
-        List<FactorValueDTO> factorValueDTOList = new ArrayList<>();
-        for (FactorValue factorValue : list) {
-
-            FactorValueDTO factorValueDTO = new FactorValueDTO();
-            // 拷贝对象
-            BeanUtils.copyProperties(factorValue, factorValueDTO);
-            factorValueDTOList.add(factorValueDTO);
-        }
-
+    // 获取所有因子值表中隐患点Id
+    // TODO 改为Mapper查询ID
+    @Override
+    public Set<Integer> getAllHideId() {
+        Set<Integer> allHideId = factorValueMapper.getAllHideId();
         // 返回hideId 列表
-        return factorValueDTOList;
+        return allHideId;
     }
 
     // 获取因子列表值
