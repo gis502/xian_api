3768255fe8a8c5ba856a1563d488d46e05cea388
chocoCui <1017185771@qq.com>
2025-08-07 22:26:37 +0800

    20250907修改bug

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/Road.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/entity/Road.java
@@ -11,49 +11,49 @@ public class Road {
 
     @TableId
     private Integer id;
-    
+
     @TableField("road_name")
     private String roadName;
-    
+
     @TableField("qdmc")
     private String qdmc;
-    
+
     @TableField("zdmc")
     private String zdmc;
-    
+
     @TableField("qctz")
     private String qctz;
-    
+
     @TableField("jzsfld")
     private String jzsfld;
-    
+
     @TableField("mqsfld")
     private String mqsfld;
-    
+
     @TableField("dlzc")
     private String dlzc;
-    
+
     @TableField("sfkzdb")
     private String sfkzdb;
-    
+
     @TableField("fxpc_sjzt")
     private String fxpcSjzt;
-    
+
     @TableField("fxpc_shlcz")
     private String fxpcShlcz;
-    
+
     @TableField("fxpc_shzt")
     private String fxpcShzt;
-    
+
     @TableField("fxpc_sshy_")
     private String fxpcSshy;
-    
+
     @TableField("fxpc_datai")
     private String fxpcDatai;
-    
+
     @TableField("geom")
     private String geom;
 
-    private String geomGeom;
+    private String pointGeom;
 
 }
