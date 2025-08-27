577e9b321b64540a47234daf7defafb2dc511f09
YQ-Bot01 <3411616574@qq.com>
2025-08-27 16:35:52 +0800

    图例

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/XianFactorAnalysisController.java
@@ -0,0 +1,43 @@
+package com.ruoyi.web.controller.system;
+
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.enums.DisasterType;
+import com.ruoyi.system.domain.vo.EarthquakeVo;
+import com.ruoyi.system.mapper.XianFactorAnalysisMapper;
+import com.ruoyi.system.service.IXianEarthquakeListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+
+@Validated
+@RestController
+@RequestMapping("/XianFactorAnalysis")
+
+public class XianFactorAnalysisController {
+
+    @Autowired
+    private XianFactorAnalysisMapper xianFactorAnalysisMapper;
+
+    @PostMapping("/disasterEstimation")
+    public List queryDisasterEstimation(@RequestParam(value = "disasterId") String disasterId, @RequestParam(value = "disasterTrigger") String disasterTrigger) {
+        DisasterType disasterType = DisasterType.EARTHQUAKE;
+        if ("暴雨".equals(disasterTrigger)) {
+            disasterType = DisasterType.RAINSTORM;
+        }
+
+        // 2. 字符串转 Long
+        Long disasterIdLong = Long.valueOf(disasterId);   // 或 parseLong
+        System.out.println(disasterIdLong+"disasterIdLong"+disasterType+"disasterType");
+
+        List<Map<String, Object>> disasterEstimation = xianFactorAnalysisMapper.queryDisasterEstimation(152292L,  DisasterType.RAINSTORM);
+        System.out.println(disasterEstimation+"disasterEstimation");
+        return disasterEstimation;
+    }
+
+
+}
