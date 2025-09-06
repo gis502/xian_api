ad0848289f01b0de2fe4be86647c95d2f70af32b
wzy-warehouse <18135009705@163.com>
2025-09-06 19:27:33 +0800

    完成承载体信息提取页面

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/XianImpactInAreaMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/XianImpactInAreaMapper.java
@@ -3,14 +3,16 @@ package com.ruoyi.system.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ruoyi.system.domain.entity.XianImpactInAreaEntity;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-
 import java.util.List;
 
 
 @Mapper
 public interface XianImpactInAreaMapper extends BaseMapper<XianImpactInAreaEntity> {
+    List<XianImpactInAreaEntity> queryPeople(Long disasterId);
 
-    
+    List<XianImpactInAreaEntity> queryTraffic(Long disasterId);
+
+    List<XianImpactInAreaEntity> queryDanger(Long disasterId);
+
+    List<XianImpactInAreaEntity> queryStation(Long disasterId);
 }
