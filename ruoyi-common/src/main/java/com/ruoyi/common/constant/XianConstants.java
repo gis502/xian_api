5f238b8c75f828c8a8aeefa9aa306084430c8e30
chocoCui <1017185771@qq.com>
2025-09-08 07:56:24 +0800

    20250908部署配置

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/XianConstants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/XianConstants.java
@@ -19,6 +19,7 @@ public class XianConstants {
     public static final String AUTH_ERROR = "授权失败!";
     public static final String TRIGGER_ERROR = "地震触发异常!";
     public static final String THEMATIC_EMPTY = "正在生成专题图,请等待...";
+    public static String IP = "http://10.22.245.247";
 
     public static final boolean SEISMIC_TYPE = false;
     public static final boolean STORM_TYPE = true;
@@ -50,22 +51,22 @@ public class XianConstants {
     }
 
     // 触发模型接口
-    public static final String BAYES_NET_MODEL_URL = "http://localhost:8085/model/bayes/prediction";
+    public static final String BAYES_NET_MODEL_URL = "http://10.22.245.246:8085/model/bayes/prediction";
     // 修改模型参数接口
-    public static final String BAYES_NET_MODEL_UPDATE_URL = "http://localhost:8085/model/bayes/change";
+    public static final String BAYES_NET_MODEL_UPDATE_URL = "http://10.22.245.246:8085/model/bayes/change";
 
     // 灾评系统授权
-    public static final String AUTH_URL = "http://localhost:18008/api/open/auth";
+    public static final String AUTH_URL = IP+":18008/api/open/auth";
 
     // 地震触发接口
-    public static final String TRIGGER_EARTHQUAKE_URL = "http://localhost:18008/api/open/eq/trigger";
+    public static final String TRIGGER_EARTHQUAKE_URL = IP+":18008/api/open/eq/trigger";
     // 暴雨触发接口
-    public static final String TRIGGER_STORM_URL = "http://localhost:18008/api/open/rain/trigger";
+    public static final String TRIGGER_STORM_URL = IP+":18008/api/open/rain/trigger";
 
     // 专题图接口
-    public static final String THEMATIC_MAP_URL = "http://localhost:18008/api/open/eq/getMap";
+    public static final String THEMATIC_MAP_URL = IP+":18008/api/open/eq/getMap";
     // 灾情报告接口
-    public static final String DISASTER_REPORT_URL = "http://localhost:18008/api/open/eq/getMap";
+    public static final String DISASTER_REPORT_URL = IP+":18008/api/open/eq/getMap";
 
 
 
