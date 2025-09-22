ac8179ed4d793b140753f0d8a4a4ccf2c184218e
Qiuhuawa <1010565096@qq.com>
2025-09-22 14:02:18 +0800

    cc20250922-灾害链模型库-添加地震灾害链整体逻辑

--- ruoyi-system/src/main/java/com/ruoyi/system/service/IDisasterChainService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/IDisasterChainService.java
@@ -2,13 +2,16 @@ package com.ruoyi.system.service;
 
 import com.ruoyi.system.domain.entity.FactorAnalysis;
 import com.ruoyi.system.domain.entity.XianDisasterRain;
+import com.ruoyi.system.domain.entity.XianEarthquakeList;
 
 import java.util.List;
 
 public interface IDisasterChainService {
     public List<XianDisasterRain> getAllRainChain();
 
-    public List<FactorAnalysis> getAllRainDisasterProbability(Integer DisasterId);
+    public List<XianEarthquakeList> getAllEarthquakeList();
+
+    public List<FactorAnalysis> getEarthQuakeProbabilityByType(Long DisasterId, String disasterType);
 
     public List<FactorAnalysis> getRainProbabilityByType(Long DisasterId, String disasterType);
 
